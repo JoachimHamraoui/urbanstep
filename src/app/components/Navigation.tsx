c1954c49997b3a12ae36ec452eb306f4ab264c25
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-07-28 10:26:45 +0200

    feat(checkout): added CartContext for context use accross all other components where needed

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { useState } from "react";
 import { FaCartShopping } from "react-icons/fa6";
+import { useCart } from "../context/CartContext";
+import  CheckoutButton  from "./CheckoutButton";
 
 export default function Navigation() {
   const [search, setSearch] = useState("");
+  const { cart } = useCart();
+  const itemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <nav className="w-full bg-neutral-800 border-b shadow-sm sticky top-0 z-10">
       <div className="max-w-7xl mx-auto flex items-center justify-between p-4 gap-4">
-        <div className="text-2xl font-bold tracking-tight uppercase font-sans">UrbanStep_</div>
+        <div className="text-2xl font-bold tracking-tight uppercase font-sans">
+          UrbanStep_
+        </div>
         <div className="flex-1 flex justify-center">
           <input
             type="text"
@@ -17,8 +23,14 @@ export default function Navigation() {
             className="w-full max-w-md px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-black"
           />
         </div>
-        <button className="relative p-2 rounded hover:text-red-500 hover:cursor-pointer transition duration-300 ease-in-out">
+        <CheckoutButton />
+        <button className="relative p-2">
           <FaCartShopping />
+          {itemCount > 0 && (
+            <span className="absolute top-0 right-0 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+              {itemCount}
+            </span>
+          )}
         </button>
       </div>
     </nav>
